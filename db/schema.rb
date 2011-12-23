956717c05408f9d58135da23441a931cc17a1f32
kiwi.win <547880119@qq.com>
2011-12-22 21:49:33 -0800

    Task I1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222115926) do
+ActiveRecord::Schema.define(:version => 20111223051658) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(:version => 20111222115926) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       :precision => 8, :scale => 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
