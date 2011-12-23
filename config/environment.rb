de184048e147e520e2cefbf27f23d7ba7ef8fbc9
kiwi.win <547880119@qq.com>
2011-12-22 19:17:42 -0800

    Task H: Mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+
+# Mail action
+Depot::Application.configure do 
+	config.action_mailer.delivery_method = :test
+end
